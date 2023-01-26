4958e749755f3e0508c936633e7f27b1bc8e88c2
javier romero <romerofj26@gmail.com>
2023-01-26 20:32:45 -0300

    creo archivo README y agrego seccion SUBIR INFORMES

---
+++ README.md
@@ -0,0 +1,9 @@
+El usuario podrá:
+
+ - Visualizar los servicios que NUEVA MEDICINA tiene para ofrecer (actualmente funcionando para comra de alimentos y registro de informes)
+ - Comprar productos "Alimentacion enteral".
+ - Agregar productos a un "carrito" de compras.
+ - Listar los productos de su "carrito".
+ - Remover productos del "carrito".
+ - Ver un resumen de su compra (artículo + precio + total de su compra).
+ 
---
+++ js/registrarInformes.js
@@ -0,0 +1,97 @@
+let tasks = [];
+
+const taskForm = document.getElementById('task-form')
+const title = document.getElementById('title');
+const description = document.getElementById("description");
+
+
+const taskList = document.getElementById('task-list')
+
+
+const createTask = (titleTask, descriptionTask) => {
+    return {
+        title: titleTask,
+        description: descriptionTask
+    }
+};
+
+const addTask = (task) => {
+    tasks.push(task)
+    const div = document.createElement('div')
+
+    div.innerHTML = `
+        <div class="card text-center mb-4">
+            <div class="card-body">
+                <strong>Título</strong>: ${task.title} -
+                <strong>Descripción</strong>: ${task.description}
+                <button href="#" class="btn btn-danger" id="${task.title}" name="delete" value="title">Delete</button>
+            </div>
+        </div>
+    `
+    taskList.appendChild(div)
+
+    saveTaskStorage(tasks)
+};
+
+const saveTaskStorage = (tasks) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+};
+
+const getTaskStorage = () => {
+    const tasksStorage = JSON.parse(localStorage.getItem('tasks'))
+    return tasksStorage
+};
+
+const showTasks = (tasks) => {
+    console.log(tasks)
+
+    const div = document.createElement('div')
+
+    // Limpiar contenedor de informes
+    taskList.innerHTML = ''
+
+    tasks.forEach(task => {
+        div.innerHTML += `
+            <div class="card card-informes text-center mb-4">
+                <div class="card-body">
+                    <strong>Paciente: </strong>: ${task.title} -
+                    <strong>Descripción</strong>: ${task.description}
+                    <button href="#" class="btn-form btn btn-danger" id="${task.title}" name="delete" value="${task.title}">Delete</button>
+                </div>
+            </div>
+        `
+        taskList.appendChild(div)
+    })
+};
+
+const deleteTask = (title) => {
+    tasks.forEach((task, index) => {
+        if (task.title === title) {
+            tasks.splice(index, 1)
+        }
+    })
+    showTasks(tasks)
+    saveTaskStorage(tasks)
+};
+
+// Listener
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('tasks')) {
+        tasks = getTaskStorage()
+        showTasks(tasks)
+    }
+})
+
+taskList.addEventListener('click', (e) => deleteTask(e.target.value))
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const titleTask = title.value;
+    const descriptionTask = description.value;
+
+    const task = createTask(titleTask, descriptionTask);
+
+    addTask(task)
+});
